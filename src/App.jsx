33d3be7d75dcaf2c930a066547db20b1a7fafb75
Rxshi2003 <rushikeshc2003@gmail.com>
2025-09-09 21:30:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+   <dev className="text-3xl font-bold text-red-500">
+    hello World lets build  netflix-gpt
+   </dev> 
+  )
+}
+
+export default App
